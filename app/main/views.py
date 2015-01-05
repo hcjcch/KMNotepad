d1d6a4734ac1cccab384af6ed3593634cfa5cdc5
kweima <zhcnmk@gmail.com>
2015-01-05 17:46:27 +0800

    Login/Logout done.

--- app/main/views.py
+++ app/main/views.py
@@ -23,32 +23,3 @@ def index():
 							form=form, name=session.get('name'),
 							known=session.get('known', False))
 
-"""
-@main.route('/', methods=['GET', 'POST'])
-def index():
-    form = NameForm()
-    if form.validate_on_submit():
-    	user = User.query.filter_by(username=form.name.data).first()
-    	if user is None:
-    		user = User(username = form.name.data)
-    		db.session.add(user)
-    		session['known'] = False
-    	else:
-    		session['known'] = True
-    	
-    	session['name'] = form.name.data 
-    	form.name.data = ''
-    	'''
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
-        session['name'] = form.name.data
-        form.name.data = None
-        '''
-        return redirect(url_for('index'))
-    return render_template('index.html', 
-    	current_time=datetime.utcnow(), 
-    	form=form, 
-    	name=session.get('name'), 
-    	known=session.get('known', False))
-"""
